299abe7119cd244dfd554b0c95d0fa4c0e283d8d
Arya <arya.atighehchian@gmail.com>
2018-10-21 19:02:34 -0400

    added toolbar to home page

--- app/src/main/java/coveros/com/trainingapp/HomeActivity.java
+++ app/src/main/java/coveros/com/trainingapp/HomeActivity.java
@@ -2,6 +2,7 @@ package coveros.com.trainingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
